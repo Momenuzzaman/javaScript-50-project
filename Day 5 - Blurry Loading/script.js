0a8e3644958a21802e77aa3b551bee03ca9d79ed
mdemon420 <mdeamon4@gmail.com>
2022-10-17 20:32:12 +0600

    clear interval

--- Day 5 - Blurry Loading/script.js
+++ Day 5 - Blurry Loading/script.js
@@ -6,7 +6,14 @@ let load = 0;
 
 const blurring = () => {
     load++;
-
+    if (load > 99) {
+        clearInterval(int);
+    }
+    loadText.textContent = `${load}%`;
+    loadText.style.opacity = scale(load, 0, 100, 1, 0);
     console.log(load);
 };
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+}
 let int = setInterval(blurring, 30);
