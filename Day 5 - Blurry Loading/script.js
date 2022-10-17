8f5483cc44a0a6746b8b0d2c0456370e25cf11d7
mdemon420 <mdeamon4@gmail.com>
2022-10-17 20:46:04 +0600

    complte

--- Day 5 - Blurry Loading/script.js
+++ Day 5 - Blurry Loading/script.js
@@ -11,7 +11,7 @@ const blurring = () => {
     }
     loadText.textContent = `${load}%`;
     loadText.style.opacity = scale(load, 0, 100, 1, 0);
-    console.log(load);
+    bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
 };
 const scale = (num, in_min, in_max, out_min, out_max) => {
     return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
