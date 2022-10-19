85790d8d8b3adc9d87402838f2c2a5d2123a380d
mdemon420 <mdeamon4@gmail.com>
2022-10-19 13:56:54 +0600

    complete

--- Day 9 - Sound Board/script.js
+++ Day 9 - Sound Board/script.js
@@ -1,10 +1,22 @@
 const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
 
-sounds.map((sound) => {
+sounds.forEach((sound) => {
     const btn = document.createElement('button');
     btn.classList.add('btn');
 
     btn.textContent = sound;
-    btn.onclick = () => console.log('click');
+    btn.addEventListener('click', () => {
+        stopSongs();
+        document.getElementById(sound).play();
+    });
     document.getElementById('buttons').appendChild(btn);
 });
+
+function stopSongs() {
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound);
+
+        song.pause();
+        song.currentTime = 0;
+    });
+}
