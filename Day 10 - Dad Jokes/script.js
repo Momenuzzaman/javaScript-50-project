55356ef27fdb73019eb0966bfd18f7c9b0d95af6
mdemon420 <mdeamon4@gmail.com>
2022-10-22 14:02:30 +0600

    async await

--- Day 10 - Dad Jokes/script.js
+++ Day 10 - Dad Jokes/script.js
@@ -3,15 +3,30 @@ const jokeBtn = document.getElementById('jokeBtn');
 
 
 
-const jokeGenerator = () => {
+async function jokeGenerator() {
     const config = {
         headers: {
             Accept: 'application/json',
         },
     };
-    fetch('https://icanhazdadjoke.com', config)
-        .then(res => res.json())
-        .then(data => console.log(data));
-};
+    const res = await fetch('https://icanhazdadjoke.com', config)
+    const data = await res.json();
+    jokeEl.innerHTML = data.joke;
+}
+
+
+
+// const jokeGenerator = () => {
+//     const config = {
+//         headers: {
+//             Accept: 'application/json',
+//         },
+//     };
+//     fetch('https://icanhazdadjoke.com', config)
+//         .then(res => res.json())
+//         .then(data => {
+//             joke.innerHTML = data.joke
+//             });
+// // };
 
 jokeBtn.addEventListener('click', jokeGenerator);
