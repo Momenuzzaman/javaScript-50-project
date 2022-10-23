daddc0ee691361a6ea69b8b4ebbba1f7a6fcdeb7
mdemon420 <mdeamon4@gmail.com>
2022-10-23 12:49:40 +0600

    create

--- Day 11 - Event KeyCodes/script.js
+++ Day 11 - Event KeyCodes/script.js
@@ -2,7 +2,7 @@
 const insert = document.getElementById('insert');
 
 window.addEventListener('keydown', (event) => {
-    insert.innerHTML = `
+  insert.innerHTML = `
   <div class="key">
   ${event.key === ' ' ? 'Space' : event.key} 
   <small>event.key</small>
