3234bb4f3c08319d0ac1de4295f00b442063c29a
mdemon420 <mdeamon4@gmail.com>
2022-11-01 17:21:33 +0600

    complete

--- Day 16 - Drink Water/script.js
+++ Day 16 - Drink Water/script.js
@@ -33,5 +33,15 @@ function updateBigCup() {
     else {
         percentage.style.visibility = 'visible';
         percentage.style.height = `${fullCups / totalCups * 330}px`;
+        percentage.innerText = `${fullCups / totalCups * 100}%`;
+    }
+
+    if (fullCups === totalCups) {
+        remained.style.visibility = 'hidden';
+        remained.style.height = 0;
+    }
+    else {
+        remained.style.visibility = 'visible';
+        liters.innerText = `${2 - (250 * fullCups / 1000)}L`;
     }
 }
